163f3d1131c1108019f20d9cbb9e4e93c5fe0118
DisableGraphics <elchifladod@gmail.com>
2022-04-21 13:31:04 +0200

    Changed from libzip-plus-plus to libarchive

--- src/zipextract.hpp
+++ src/zipextract.hpp
@@ -1,9 +1,10 @@
-#include "zip.hpp"
 #include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <filesystem>
+#include <archive.h>
+#include <archive_entry.h>
 
 inline std::string reverse_string(std::string s)
 {
@@ -17,25 +18,84 @@ inline std::string reverse_string(std::string s)
 
 namespace zipe
 {
-    void extract(std::string filename, std::string folder)
+    
+    static int copy_data(struct archive *ar, struct archive *aw)
     {
-        libzip::archive archive(filename);
-        
+        int r;
+        const void *buff;
+        size_t size;
+        la_int64_t offset;
 
-        for(auto & file : archive)
-        {
-            std::string file_n{file.name};
-            if(file_n[file_n.size() - 1] == '/')
-            {
-                std::filesystem::create_directory(folder + file.name);
-            }
-            else {
-                libzip::stat_info stat = archive.stat(file.name);
-                libzip::file file_t = archive.open(file.name);
-                std::ofstream out{folder + file.name};
-                out << file_t.read(stat.size);
-                out.close();
+        for (;;) {
+            r = archive_read_data_block(ar, &buff, &size, &offset);
+            if (r == ARCHIVE_EOF)
+                return (ARCHIVE_OK);
+            if (r < ARCHIVE_OK)
+                return (r);
+            r = archive_write_data_block(aw, buff, size, offset);
+            if (r < ARCHIVE_OK) {
+                fprintf(stderr, "%s\n", archive_error_string(aw));
+                return (r);
             }
         }
+    }
+
+    inline void extract(std::string filename, std::string folder)
+    {
+        struct archive *a;
+        struct archive *ext;
+        struct archive_entry *entry;
+        int flags;
+        int r;
+
+        /* Select which attributes we want to restore. */
+        flags = ARCHIVE_EXTRACT_TIME;
+        flags |= ARCHIVE_EXTRACT_PERM;
+        flags |= ARCHIVE_EXTRACT_ACL;
+        flags |= ARCHIVE_EXTRACT_FFLAGS;
+
+        a = archive_read_new();
+        archive_read_support_format_all(a);
+        archive_read_support_compression_all(a);
+        ext = archive_write_disk_new();
+        
+        archive_write_disk_set_options(ext, flags);
+        archive_write_disk_set_standard_lookup(ext);
+        if ((r = archive_read_open_filename(a, filename.c_str(), 10240)))
+            exit(1);
+        for (;;) {
+            r = archive_read_next_header(a, &entry);
+            if (r == ARCHIVE_EOF)
+                break;
+            if (r < ARCHIVE_OK)
+                fprintf(stderr, "%s\n", archive_error_string(a));
+            if (r < ARCHIVE_WARN)
+                exit(1);
+
+            const char* currentFile = archive_entry_pathname(entry);
+            const std::string fullOutputPath = folder + currentFile;
+            archive_entry_set_pathname(entry, fullOutputPath.c_str());
+            r = archive_write_header(ext, entry);
+            if (r < ARCHIVE_OK)
+                fprintf(stderr, "%s\n", archive_error_string(ext));
+            else if (archive_entry_size(entry) > 0) {
+                r = copy_data(a, ext);
+            if (r < ARCHIVE_OK)
+                fprintf(stderr, "%s\n", archive_error_string(ext));
+            if (r < ARCHIVE_WARN)
+                exit(1);
+            }
+            r = archive_write_finish_entry(ext);
+            if (r < ARCHIVE_OK)
+                fprintf(stderr, "%s\n", archive_error_string(ext));
+            if (r < ARCHIVE_WARN){
+                std::cout << "[ERROR]: Malformed file";
+                exit(1);
+            }
+        }
+        archive_read_close(a);
+        archive_read_free(a);
+        archive_write_close(ext);
+        archive_write_free(ext);
     }
 }
